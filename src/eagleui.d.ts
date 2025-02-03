7a92147c401378de42192bf089b491a41ad44f8f
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-02-03 23:34:32 +0100

    feat: remove Timestamp component and its associated types

--- src/eagleui.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './components/Button/Button.types';
-export * from './components/Dialog/Dialog.types';
-export * from './components/Input/Input.types';
-export * from './components/Select/Select.types';
-export * from './components/Timestamp/Timestamp.types';
--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,3 @@ export { Dialog } from './components/Dialog/Dialog';
 export { Gradient } from './components/Gradient/Gradient';
 export { Input } from './components/Input/Input';
 export { Select } from './components/Select/Select';
-export { TimeStamp } from './components/Timestamp/Timestamp';
-export type * from './eagleui.d';

bae1f64adef71354b912ef7cf32e73f9c46437e1
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-02-07 13:28:35 +0100

    feat: enhance ButtonGroup types; add Slider and Textarea component type definitions

--- src/components/ButtonGroup/ButtonGroup.types.ts
+++ src/components/ButtonGroup/ButtonGroup.types.ts
@@ -1,20 +1,38 @@
+import { LucideIcon } from "lucide-react";
+
 /**
  * Props for the ButtonGroup component.
  */
 export interface ButtonGroupProps {
 	/**
-	 * An array of button labels.
+	 * Array of Button objects to be rendered in the ButtonGroup.
 	 */
-	buttons: string[];
+	buttons: Button[];
 
 	/**
-	 * The label of the currently active button.
+	 * Identifier for the currently active button.
 	 */
 	activeButton: string;
 
 	/**
-	 * Callback function to handle button click events.
-	 * @param button - The label of the clicked button.
+	 * Callback function to be called when a button is clicked.
+	 * @param button - The identifier of the clicked button.
 	 */
 	onButtonClick: (button: string) => void;
+
+	/**
+	 * Size of the buttons in the ButtonGroup. Can be 'sm' (small), 'md' (medium), or 'lg' (large).
+	 */
+	size?: 'sm' | 'md' | 'lg';
+
+	/**
+	 * Additional CSS class names to apply to the ButtonGroup.
+	 */
+	className?: string;
+}
+
+
+interface Button {
+	label: string;
+	icon?: LucideIcon;
 }
